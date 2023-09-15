c22a2ef5ba3329f75d18e071bfb278cc9f23f843
Masato Lin <masatoli@usc.edu>
2023-09-15 15:53:47 -0700

    added obstacle

---
+++ CS526Prototype/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] private Transform[] path;
+    private int index = 0;
+    [SerializeField] private float speed = 7.0f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 directionToDestination = path[index].position - transform.position;
+        if (directionToDestination.magnitude > 1)
+        {
+            directionToDestination.Normalize();
+            transform.position += directionToDestination * speed * Time.deltaTime;
+        }
+        else
+        {
+            index++;
+            if (index >= path.Length)
+            {
+                index = 0;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance.playerHealth.Die();
+        }
+
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
