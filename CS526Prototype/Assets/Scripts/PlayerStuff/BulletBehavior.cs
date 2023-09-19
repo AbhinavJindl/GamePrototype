49f4050182bb0620f636008e43c662657cf3ae74
Masato Lin <masatoli@usc.edu>
2023-09-18 17:13:00 -0700

    pushed bullet logic for better bulelt detection

--- CS526Prototype/Assets/Scripts/PlayerStuff/BulletBehavior.cs
+++ CS526Prototype/Assets/Scripts/PlayerStuff/BulletBehavior.cs
@@ -8,25 +8,42 @@ public class BulletBehavior : MonoBehaviour
     [SerializeField] private string wallTag;
     [SerializeField] private string bounceTag;
     [SerializeField] private string lethalTag;
+    private bool isBouncing = false;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (wallTag != "" && col.gameObject.CompareTag(wallTag))
+        StartCoroutine(HandleBulletStuff(col));
+    }
+
+    IEnumerator HandleBulletStuff(Collider2D col)
+    {
+        if (bounceTag != "" && col.gameObject.CompareTag(bounceTag))
         {
-            Destroy(gameObject);
-        }
-        else if (bounceTag != "" && col.gameObject.CompareTag(bounceTag))
-        {
+            isBouncing = true;
             Vector3 closestPoint = col.ClosestPoint(transform.position);
             Vector3 normal = transform.position - closestPoint;
             normal.Normalize();
             Vector3 reflectVec = transform.up - 2 * Vector3.Dot(transform.up, normal) * normal;
             transform.up = reflectVec;
         }
+        else if (wallTag != "" && col.gameObject.CompareTag(wallTag))
+        {
+            yield return new WaitForSeconds(0.01f);
+            if (!isBouncing)
+            {
+                Destroy(gameObject);
+            }
+        }
         else if (lethalTag != "" && col.gameObject.CompareTag(lethalTag))
         {
-            GameManager.Instance.Die();
-            Destroy(gameObject);
+            yield return new WaitForSeconds(0.01f);
+            if (!isBouncing)
+            {
+                GameManager.Instance.Die();
+                Destroy(gameObject);
+            }
         }
+
+        yield return null;
     }
 }
